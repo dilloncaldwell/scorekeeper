e028831b82b098a00de09780f771b5c73b3d1d30
Dillon Caldwell <dcaldwell1228@gmail.com>
2022-06-24 16:05:58 -0400

    Style and layout of match stats popup

---
+++ js/functions/Game.js
@@ -0,0 +1,13 @@
+export default class Game {
+	constructor() {
+		this.winner = "";
+		this.p1points = 0;
+		this.p2points = 0;
+		this.p1fouls = 0;
+		this.p2fouls = 0;
+		this.p1defense = 0;
+		this.p2defense = 0;
+		this.p1timeouts = 0;
+		this.p2timeouts = 0;
+	}
+}
